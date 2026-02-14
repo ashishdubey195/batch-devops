9057a76fd59d1224f2725abcd6d4d86feda8d319
ashish <ashishdubey195@gmail.com>
2026-02-14 11:44:35 +0000

    let ashish-vpc change be there

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
 print("Hello WORLD!")
 
 print('ashish')
-
-print("This is collabrator change")
-
-<<<<<<< HEAD
-print("i have added by ashish")
-=======
 print("i have added by ashish-vpc-account")
->>>>>>> 0459fe4716092e4fdf19d979bf1c7d1c60189fec
-
+print("This is collabrator change")  
 a = 6
 
